e79fda1f7ed4f887c60430d4ac09e21985622401
SnowLeopardJB <snowleopardjb@gmail.com>
2013-04-12 14:01:10 -0400

    Remove built files from repository

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Initial compile"
 ./apktool b $1
-echo "Copying AndroidManifest-Original to apk"
+echo "Copying original files to apk"
 cp $1/AndroidManifest-Original.xml $1/build/apk/AndroidManifest.xml
+cp -r $1/META-INF $1/build/apk/META-INF
 echo "Secondary compile"
 ./apktool b $1
 echo "Done!"
